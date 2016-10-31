12acb6e7f279ea450bfbf53cfbc37534b9520fb9
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-31 13:59:41 +0100

    Added a third line from dev branch

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("I added this line from dev branch!");
 		System.out.println("I added this line from master branch!");
+		System.out.println("This line I added from dev branch just to have something to merge");
 	}
 
 }
