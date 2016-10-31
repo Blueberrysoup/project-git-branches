ad0f56a6609f274829c495d01fcd5f7218ffe587
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-31 13:51:08 +0100

    Third commit from master branch. I added a second output line

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("I added this line from dev branch!");
-
+		System.out.println("I added this line from master branch!");
 	}
 
 }
