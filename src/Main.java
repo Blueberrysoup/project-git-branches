1d6d46ee46f811730f06689d547a741eadae3b63
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-31 14:14:04 +0100

    creating a conflict

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("I added this line from dev branch!");
-		System.out.println("I added this line from master branch!");
+		System.out.println("Changing here in master just to get a conflict!");
 		System.out.println("This line I added from dev branch just to have something to merge");
 	}
 
