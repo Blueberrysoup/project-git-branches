013a78b6155bc0aa30e7ab396308df349686522b
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-31 20:13:20 +0100

    MASTER CHANGE

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		System.out.println("I added this line from dev branch!");
 		System.out.println("Changing here in master just to get a conflict!");
 		System.out.println("I added this line from master branch!");
-		System.out.println("Added from dev and will try to merge just to get a conflict");
+		System.out.println("MASTER CHANGE");
 		System.out.println("This line I added from dev branch just to have something to merge");
 	}
 
